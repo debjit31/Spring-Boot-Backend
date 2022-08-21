6c3aaadc44ddf64fbc97dc7f28e137f9c3914ab5
debjit31 <debjitchatterjee31@gmail.com>
2022-08-21 14:55:55 +0530

    Setting up the Project Structure

---
+++ src/test/java/com/BloodDonationApp/bloodDonationApp/BloodDonationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BloodDonationApp.bloodDonationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
